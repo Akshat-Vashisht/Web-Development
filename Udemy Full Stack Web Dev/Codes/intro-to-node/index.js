89786da16eecfdbeebda5729ee4bcbf46792ef64
Akshat-Vashisht <akashat2003@gmail.com>
2023-05-29 17:23:46 +0530

    Started backend part

---
+++ Udemy Full Stack Web Dev/Codes/intro-to-node/index.js
@@ -0,0 +1,15 @@
+// Using internal node modules
+
+// const fs = require("fs")
+
+// fs.copyFileSync("test1.txt","copyOftest1.txt")
+
+
+
+// Using external node modules
+
+const supervillains = require('supervillains');
+
+var mySuperVillain = supervillains.random()
+
+console.log("Your superhero name is " + mySuperVillain)
